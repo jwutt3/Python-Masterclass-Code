8f2238d6b6ee580878a80fdad8c251fca68aaa61
James Utt <jwu3ma@gmail.com>
2017-04-18 17:38:52 -0400

    more code from OO section of course

--- Game/enemy.py
+++ Game/enemy.py
@@ -4,6 +4,7 @@ class Enemy:
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
@@ -12,6 +13,11 @@ class Enemy:
             print("I took {} points damage and have {} left".format(damage, self.hit_points))
         else:
             self.lives -= 1
+            if self.lives > 0:
+                print("{0.name} lost a life".format(self))
+            else:
+                print("{0.name} is dead".format(self))
+                self.alive = False
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
@@ -24,3 +30,9 @@ class Troll(Enemy):
 
     def grunt(self):
         print("Me {0.name}. {0.name} stomp you".format(self))
+
+class Vampire(Enemy):
+
+    def __init__(self, name):
+        super().__init__(name=name, lives=3, hit_points=12)
+
--- Game/main.py
+++ Game/main.py
@@ -1,4 +1,4 @@
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampire
 
 ugly_troll = Troll("Pug")
 print("Ugly troll - {}".format(ugly_troll))
@@ -9,7 +9,20 @@ print("Another troll - {}".format(another_troll))
 brother_troll = Troll("Urg")
 print("Brother troll - {}".format(brother_troll))
 
+basic_vampire = Vampire("Vlad")
+print("Vlad - {}".format(basic_vampire))
+
+basic_vampire.take_damage(7)
+
+print("Vlad - {}".format(basic_vampire))
+
 ugly_troll.grunt()
 another_troll.grunt()
 brother_troll.grunt()
+
+print("-" * 40)
+
+while basic_vampire.alive:
+    basic_vampire.take_damage(1)
+    print(basic_vampire)
 
